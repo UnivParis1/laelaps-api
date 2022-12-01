d679bee09ad6c773edc3a5845cd5df10f91b6d44
jdp1ps <joadorn@gmail.com>
2022-12-01 14:08:28 +0100

    Enforce types

--- front/index.ts
+++ front/index.ts
@@ -1,25 +1,27 @@
 import axios, {AxiosResponse} from "axios";
 import * as tf from "tableify";
 
-let running = false, jobId = null, textArea, parseButton, parseButtonIcon, clearButton, preloadIcon,
+let running: boolean = false, jobId: string, textArea: HTMLInputElement | null,
+    parseButton: HTMLButtonElement | null, parseButtonIcon: HTMLImageElement | null,
+    clearButton: HTMLButtonElement | null,
     resultContainer = null;
 
 
-const isEmpty = () => {
+const isEmpty = (): boolean => {
     return !!textArea.value.match(/^\s*$/);
 }
 
 const POLLING_DELAY = 1000
 
-function pollServer() {
+const pollServer = (): void => {
     if (!jobId) return;
-    setTimeout(askForJobState, POLLING_DELAY)
-}
+    setTimeout(updateJobState, POLLING_DELAY)
+};
 
-const askForJobState = () => {
+const updateJobState = (): void => {
     axios.get<AxiosResponse>(`/api/parsing/${jobId}`, {params: {reference: textArea.value}})
-        .then(function (response) {
-            jobId = response.data;
+        .then(response => {
+            jobId = response.data.toString();
             if (response.data) {
                 resultContainer.innerHTML = tf(response.data['parsing']);
                 resultContainer.querySelectorAll("table")[0].classList.add("table", "table-striped")
@@ -29,25 +31,23 @@ const askForJobState = () => {
             }
             pollServer();
         })
-        .catch(function (error) {
+        .catch(error => {
             // handle error
             console.log(error);
         })
 }
-const updateButtonState = () => {
+const updateButtonState = (): void => {
     parseButton.disabled = isEmpty();
     clearButton.disabled = isEmpty();
     parseButtonIcon.style.visibility = running ? "hidden" : "visible"
-    preloadIcon.style.visibility = !running ? "hidden" : "visible"
 };
 
 
 document.addEventListener("DOMContentLoaded", function () {
-    textArea = document.getElementById("reference");
-    parseButton = document.getElementById("parse-button");
-    clearButton = document.getElementById("clear-button");
-    preloadIcon = document.getElementById("parse-preload");
-    parseButtonIcon = parseButton.querySelectorAll('.glyphicon')[0];
+    textArea = <HTMLInputElement>document.getElementById("reference");
+    parseButton = <HTMLButtonElement>document.getElementById("parse-button");
+    clearButton = <HTMLButtonElement>document.getElementById("clear-button");
+    parseButtonIcon = <HTMLImageElement>parseButton.querySelectorAll('.glyphicon')[0];
     resultContainer = document.getElementById("result-container");
     textArea.addEventListener("input", () => {
         updateButtonState();
